877100b3d221e1bac2e929ea8f5910751ea621d7
kido <baekido@gmail.com>
2022-04-18 17:28:33 +0900

    add invalid terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+ resource "aws_elasticache_replication_group" "bad_example" {
+         replication_group_id = "foo"
+         replication_group_description = "my foo cluster"
+
+         at_rest_encryption_enabled = false
+ }
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
 }
