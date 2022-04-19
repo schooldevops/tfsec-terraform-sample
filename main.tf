23e2821946c5fa5ed4a416f26976dfe331d62083
kido <baekido@gmail.com>
2022-04-19 18:06:38 +0900

    add

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_elasticache_replication_group" "bad_example" {
          replication_group_id = "foo"
          replication_group_description = "my foo cluster"
-         transit_encryption_enabled = false
-         at_rest_encryption_enabled = false
+         transit_encryption_enabled = true
+         at_rest_encryption_enabled = true
  }
 
 output "jenkins_terraform" {
