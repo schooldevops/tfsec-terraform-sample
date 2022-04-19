627c0838be387243e7a7bbf8f6d8d1f16e6882a2
kido <baekido@gmail.com>
2022-04-19 16:21:37 +0900

    add

--- tfsecw.sh
+++ tfsecw.sh
@@ -14,13 +14,13 @@ echo "$(pwd)"
 
 curl -fsSL https://github.com/aquasecurity/tfsec/releases/download/v1.18.0/tfsec_1.18.0_linux_arm64.tar.gz
 
-tar xzvf tfsec_1.18.0_linux_arm64.tar.gz --directory ./tfsec
+tar xzvf tfsec_1.18.0_linux_arm64.tar.gz --directory ./tfsec_dir
 
-ls -alt ./tfsec 
+ls -alt ./tfsec_dir 
 
-chmod 755 ./tfsec/bin/tfsec
+chmod 755 ./tfsec_dir/bin/tfsec
 
-./tfsec . -f junit > tfsec_results.xml
+./tfsec_dir . -f junit > tfsec_results.xml
 
 # ls -lat $(pwd)
 
