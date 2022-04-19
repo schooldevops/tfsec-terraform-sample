1fddc81a6576e533d3010dfc9c1b245371615a61
kido <baekido@gmail.com>
2022-04-19 14:37:32 +0900

    modify jenkins

--- tfsecw.sh
+++ tfsecw.sh
@@ -6,5 +6,6 @@ ls ./
 
 echo "----------------------- Run tfsec with docker"
 
-docker run --rm -v "$(pwd):/src" aquasec/tfsec /src --no-color -f junit > tfsec_result.xml
+# docker run --rm -v "$(pwd):/src" aquasec/tfsec /src --no-color -f junit > tfsec_result.xml
+`docker run --rm -v "$(pwd):/src" aquasec/tfsec /src -f junit` > tfsec_result.xml
 
