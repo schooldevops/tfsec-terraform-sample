46f7340993f01458688cf23d844865540c0dcffe
kido <baekido@gmail.com>
2022-04-19 16:19:57 +0900

    add

--- tfsecw.sh
+++ tfsecw.sh
@@ -12,7 +12,7 @@ echo "$(pwd)"
 # docker run --rm -v "$(pwd):/src" aquasec/tfsec /src -f junit > tfsec_results.xml
 # docker run --rm -v $(pwd)/:/src aquasec/tfsec /src --no-color
 
-curl -fsSLO https://github.com/aquasecurity/tfsec/releases/download/v1.18.0/tfsec_1.18.0_linux_arm64.tar.gz
+curl -fsSL https://github.com/aquasecurity/tfsec/releases/download/v1.18.0/tfsec_1.18.0_linux_arm64.tar.gz
 
 tar xzvf tfsec_1.18.0_linux_arm64.tar.gz --directory ./tfsec
 
