c8253f638d68752d6a4807ae615aecb4ea4ecf4b
kido <baekido@gmail.com>
2022-04-25 10:01:07 +0900

    add tfsec terraform

--- main.tf
+++ main.tf
@@ -3,8 +3,7 @@ resource "aws_elasticache_replication_group" "bad_example" {
          replication_group_description = "my foo cluster"
          transit_encryption_enabled = false
          at_rest_encryption_enabled = false
- }
-
+ } 
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
 }
